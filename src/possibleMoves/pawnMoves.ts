ad83449d1e6cd5dbd3d64a4cb0957081ef760f9f
Manicharan <manicharan150@gmail.com>
2024-11-12 09:54:25 +0530

    Added starting and promoting funcitonality

--- src/possibleMoves/pawnMoves.ts
+++ src/possibleMoves/pawnMoves.ts
@@ -2,8 +2,18 @@ import { board, previous_player } from "..";
 
 export function getPawnMoves(row: number, col: number): [number, number][] {
     const moves: [number, number][] = [];
+    let whitePromotingAvailable: boolean = false;
+    let blackPromotingAvailable: boolean = false;
 
     if (board[row][col].startsWith("white")) {
+        if (row === 6) {
+            moves.push([row - 2, col]);
+        }
+
+        if (row === 2) {
+            whitePromotingAvailable = true;
+        }
+
         if (board[row - 1][col] === "0") {
             moves.push([row - 1, col]);
         }
@@ -16,6 +26,14 @@ export function getPawnMoves(row: number, col: number): [number, number][] {
             moves.push([row - 1, col - 1]);
         }
     } else if (board[row][col].startsWith("black")) {
+        if (row === 1) {
+            moves.push([row + 2, col]);
+        }
+
+        if (row === 6) {
+            blackPromotingAvailable = true;
+        }
+
         if (board[row + 1][col] === "0") {
             moves.push([row + 1, col]);
         }
