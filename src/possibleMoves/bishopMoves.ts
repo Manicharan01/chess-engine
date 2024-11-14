4a327e4d59d2a9763f2f55429173ff4cb8b3d22a
Manicharan <manicharan150@gmail.com>
2024-11-14 12:31:32 +0530

    Now we can check if the a bishop move is legal or not

--- src/possibleMoves/bishopMoves.ts
+++ src/possibleMoves/bishopMoves.ts
@@ -1,50 +1,51 @@
 import { board } from "..";
+import { parseMove } from "../moveParser";
 
 export function getBishopMove(row: number, col: number, opponent: string, your_color: string): [number, number][] {
     const moves: [number, number][] = [];
 
-    for (let i = 0; i < 8; i++) {
+    for (let i = 1; i < 8; i++) {
         if (row + i < 8 && col + i < 8) {
-            if (board[row + i][col + i].startsWith(opponent)) {
+            if (board[row + i][col + i].startsWith(opponent + "_")) {
                 moves.push([row + i, col + i])
                 break
-            } else if (board[row + i][col + i].startsWith(your_color)) {
+            } else if (board[row + i][col + i].startsWith(your_color + "_")) {
                 break
             }
 
             moves.push([row + i, col + i]);
         }
     }
-    for (let i = 0; i < 8; i++) {
+    for (let i = 1; i < 8; i++) {
         if (row - i >= 0 && col + i < 8) {
-            if (board[row - i][col + i].startsWith(opponent)) {
+            if (board[row - i][col + i].startsWith(opponent + "_")) {
                 moves.push([row - i, col + i]);
                 break
-            } else if (board[row - i][col + i].startsWith(your_color)) {
+            } else if (board[row - i][col + i].startsWith(your_color + "_")) {
                 break
             }
 
             moves.push([row - i, col + i]);
         }
     }
-    for (let i = 0; i < 8; i++) {
+    for (let i = 1; i < 8; i++) {
         if (row + i < 8 && col - i >= 0) {
-            if (board[row + i][col - i].startsWith(opponent)) {
+            if (board[row + i][col - i].startsWith(opponent + "_")) {
                 moves.push([row + i, col - i]);
                 break
-            } else if (board[row + i][col + i].startsWith(your_color)) {
+            } else if (board[row + i][col + i].startsWith(your_color + "_")) {
                 break
             }
 
             moves.push([row + i, col - i]);
         }
     }
-    for (let i = 0; i < 8; i++) {
+    for (let i = 1; i < 8; i++) {
         if (row - i >= 0 && col - i >= 0) {
-            if (board[row - i][col - i].startsWith(opponent)) {
+            if (board[row - i][col - i].startsWith(opponent + "_")) {
                 moves.push([row - i, col - i]);
                 break
-            } else if (board[row - i][col - i].startsWith(your_color)) {
+            } else if (board[row - i][col - i].startsWith(your_color + "_")) {
                 break
             }
 
@@ -53,4 +54,30 @@ export function getBishopMove(row: number, col: number, opponent: string, your_c
     }
 
     return moves;
+}
+
+export function isMoveLegal(piecePosition: [number, number], current_move: string): boolean {
+    let legalMove: boolean = false;
+
+    if (board[piecePosition[0]][piecePosition[1]].startsWith("white_")) {
+        const bishopMoves: [number, number][] = getBishopMove(piecePosition[0], piecePosition[1], "black", "white");
+        const [row, col] = parseMove(current_move);
+
+        bishopMoves.map(([r, c]) => {
+            if (r === row && c === col) {
+                legalMove = true
+            }
+        })
+    } else if (board[piecePosition[0]][piecePosition[1]].startsWith("black_")) {
+        const bishopMoves: [number, number][] = getBishopMove(piecePosition[0], piecePosition[1], "white", "black");
+        const [row, col] = parseMove(current_move);
+
+        bishopMoves.map(([r, c]) => {
+            if (r === row && c === col) {
+                legalMove = true
+            }
+        })
+    }
+
+    return legalMove
 }
